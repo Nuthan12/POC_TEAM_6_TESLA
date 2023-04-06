5d447e39bfd2a2078112643bec7eed269c36fc4a
Santhosh Kumar A <santhosh.a@valtech.com>
2023-04-06 10:59:26 +0530

    B2B customization

---
+++ bin/custom/poct6/poct6backoffice/gensrc/org/poct6backoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 26-Mar-2023, 10:26:19 pm                    ---
+ * ----------------------------------------------------------------
+ */
+package org.poct6backoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "poct6backoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ bin/custom/poct6/poct6test/gensrc/de/hybris/poct6/test/constants/GeneratedPoct6TestConstants.java
@@ -0,0 +1,26 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 26-Mar-2023, 10:26:19 pm                    ---
+ * ----------------------------------------------------------------
+ *  
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.poct6.test.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedPoct6TestConstants
+{
+	public static final String EXTENSIONNAME = "poct6test";
+	
+	protected GeneratedPoct6TestConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
