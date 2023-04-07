5ce76b14f639ac5dcb62a7eaf9a16cd097f63d0d
Santhosh Kumar A <santhosh.a@valtech.com>
2023-04-07 17:37:51 +0530

    B2B Customization

---
+++ bin/custom/poct6/poct6backoffice/gensrc/org/poct6backoffice/constants/GeneratedYacceleratorbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 24-Mar-2023, 12:52:35 pm                    ---
+ * ----------------------------------------------------------------
+ */
+package org.poct6backoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedYacceleratorbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "poct6backoffice";
+	
+	protected GeneratedYacceleratorbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
